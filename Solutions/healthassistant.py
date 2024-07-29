13ea2c8ab63b2c9e820e40ca46eae565cf150c79
zdomke <zdomke@slac.stanford.edu>
2024-07-29 10:34:42 -0700

    init_commit

---
+++ Solutions/healthassistant.py
@@ -0,0 +1,45 @@
+
+import openai
+from openai import OpenAI
+import os
+import time
+import json
+
+with open("config.json") as f:
+    conf = json.load(f)
+
+openai.api_key = conf['api_key']
+
+
+if __name__ == '__main__':
+    os.system('clear')
+    print('Welcome to the Stanford Medical AI Assistant!')
+    print('How can I help you today?')
+    prompt = input('Enter your question here. When you are done, simply enter \'done\' to exit: ')
+    while True:
+        if prompt == 'done':
+            os.system('clear')
+            print('Thank you for visiting us. Have a Nice day! Goodbye.')
+            break
+        client = OpenAI(
+            # defaults to os.environ.get("OPENAI_API_KEY")
+            api_key=conf['client_key'],
+        )
+
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        os.system('clear')
+        print('Working on your request...')
+        time.sleep(5)
+        os.system('clear')
+        print(chat_completion.choices[0].message.content)
+        print('\n')
+        prompt = input('Do you have any additional questions?\nWhen you are done, simply enter \'done\' to exit: ')
+
